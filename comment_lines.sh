420fd29fdf4fb9712a5d51aa82e100db31ed2fd8
ANKUSHJAIN91 <57315923+ANKUSHJAIN91@users.noreply.github.com>
2024-05-18 22:56:02 +0530

    Update comment_lines.sh

--- comment_lines.sh
+++ comment_lines.sh
@@ -4,8 +4,20 @@
 target_file="/var/www/html/apps/settings/appinfo/routes.php"
 
 # Comment out the specified lines
-sed -i '/AppSettings#listCategories/ s/^/\/\/ /' "$target_file"
-sed -i '/AppSettings#viewApps/ s/^/\/\/ /' "$target_file"
-# ... (repeat for other lines)
+sed -i '/AppSettings#listCategories/ { s/^/\/\/ /; t; }' "$target_file"
+sed -i '/AppSettings#viewApps/ { s/^/\/\/ /; t; }' "$target_file"
+sed -i '/AppSettings#listApps/ { s/^/\/\/ /; t; }' "$target_file"
+sed -i '/AppSettings#enableApp/ { s/^/\/\/ /; t; }' "$target_file"
+sed -i '/AppSettings#enableApp/ { s/^/\/\/ /; t; }' "$target_file"
+sed -i '/AppSettings#enableApps/ { s/^/\/\/ /; t; }' "$target_file"
+sed -i '/AppSettings#disableApp/ { s/^/\/\/ /; t; }' "$target_file"
+sed -i '/AppSettings#disableApps/ { s/^/\/\/ /; t; }' "$target_file"
+sed -i '/AppSettings#updateApp/ { s/^/\/\/ /; t; }' "$target_file"
+sed -i '/AppSettings#uninstallApp/ { s/^/\/\/ /; t; }' "$target_file"
+sed -i '/AppSettings#viewApps/ { s/^/\/\/ /; t; }' "$target_file"
+sed -i '/AppSettings#viewApps/ { s/^/\/\/ /; t; }' "$target_file"
+sed -i '/AppSettings#force/ { s/^/\/\/ /; t; }' "$target_file"
+sed -i '/Help#help/ { s/^/\/\/ /; t; }' "$target_file"
+sed -i '/Reasons#getPdf/ { s/^/\/\/ /; t; }' "$target_file"
 
 echo "Lines commented out successfully!"
