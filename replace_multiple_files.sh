23b9675cc8c27958a73b6ac79ac4dc4dfbe5f065
ANKUSHJAIN91 <57315923+ANKUSHJAIN91@users.noreply.github.com>
2024-05-19 18:19:49 +0530

    Update replace_multiple_files.sh

--- replace_multiple_files.sh
+++ replace_multiple_files.sh
@@ -15,6 +15,17 @@ TARGET_DIRS=("/var/www/html/apps/settings/appinfo/"
             "/var/www/html/apps/settings/templates/settings/admin"
             "/var/www/html/apps/settings/templates/settings/admin"
             ) # Corresponding array of target directories
+# Arrays for directories and files to be removed
+REMOVE_DIRS=("/var/www/html/apps/firstrunwizard" )  # Directories to remove
+REMOVE_FILES=("/var/www/html/dist/federatedfilesharing-vue-settings-personal.js" 
+              "/var/www/html/dist/federatedfilesharing-vue-settings-personal.js.map"
+              "/var/www/html/dist/federatedfilesharing-vue-settings-admin.js"
+              "/var/www/html/dist/federatedfilesharing-vue-settings-admin.js.map"
+              "/var/www/html/dist/updatenotification-updatenotification.js.map"
+              "/var/www/html/dist/updatenotification-updatenotification.js"
+              "/var/www/html/dist/updatenotification-updatenotification.js.LICENSE.txt"
+              "/var/www/html/dist/settings-vue-settings-admin-basic-settings.js.map"
+              )  # Files to remove
 
 # Check if the lengths of FILES and TARGET_DIRS arrays match
 if [ ${#FILES[@]} -ne ${#TARGET_DIRS[@]} ]; then
@@ -44,6 +55,46 @@ download_and_replace() {
     exit 1
   fi
 }
+
+# Function to remove directories
+remove_directories() {
+  for dir in "${REMOVE_DIRS[@]}"; do
+    if [ -d "$dir" ]; then
+      echo "Removing directory: $dir"
+      rm -rf "$dir"
+      if [ $? -eq 0 ]; then
+        echo "Directory successfully removed: $dir"
+      else
+        echo "Failed to remove directory: $dir"
+        exit 1
+      fi
+    else
+      echo "Directory does not exist: $dir"
+    fi
+  done
+}
+
+# Function to remove files
+remove_files() {
+  for file in "${REMOVE_FILES[@]}"; do
+    if [ -f "$file" ]; then
+      echo "Removing file: $file"
+      rm -f "$file"
+      if [ $? -eq 0 ]; then
+        echo "File successfully removed: $file"
+      else
+        echo "Failed to remove file: $file"
+        exit 1
+      fi
+    else
+      echo "File does not exist: $file"
+    fi
+  done
+}
+
+# Remove specified directories and files
+remove_directories
+remove_files
 
 # Iterate over the arrays and download each file to its corresponding target directory
 for i in "${!FILES[@]}"; do
