174309aa92dfeea2cad9c22c5449d6985aa64ee9
ANKUSHJAIN91 <57315923+ANKUSHJAIN91@users.noreply.github.com>
2024-08-07 18:57:42 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import uno
+import urllib.request
+
 document = XSCRIPTCONTEXT.getDocument()
+url = 'https://n8n.jcityoffice.com/webhook/78aaa2aa-bd89-44ad-bc59-9ba8db3e7d00?Div=Div1&month=April'
 
 def write_something():
     month = "APRIL"
@@ -7,3 +10,6 @@ def write_something():
     sheets = document.getSheets()
     firstSheet = sheets.getByIndex(0)
     firstSheet.getCellRangeByName("A1").setString('hiwwqqii')
+    with urllib.request.urlopen(url) as f:
+    # print(f.read(300))
+    print(f.read())
