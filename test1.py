2cf9aa07702b4e877e5768e7ced935f0e1de229e
ANKUSHJAIN91 <57315923+ANKUSHJAIN91@users.noreply.github.com>
2024-08-08 21:38:08 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,4 +1,4 @@
-# import uno
+import uno
 import urllib.request
 from urllib.parse import urlencode
 from urllib.request import urlopen
@@ -8,26 +8,6 @@ import sys
 import requests
 
 document = XSCRIPTCONTEXT.getDocument()
-# url = 'https://n8n.jcityoffice.com/webhook/78aaa2aa-bd89-44ad-bc59-9ba8db3e7d00?Div=Div1&month=April'
-
-# def write_something():
-#     month = "APRIL"
-#     div = "Div1"
-#     sheets = document.getSheets()
-#     firstSheet = sheets.getByIndex(0)
-#     firstSheet.getCellRangeByName("A1").setString('hhhhhh')
-#     firstSheet.getCellRangeByName("A4").setString('rrrrr')
-#     try:
-#         with urllib.request.urlopen(url) as f:
-#             # print(f.read(300))
-#             print(f.read())
-#             firstSheet.getCellRangeByName("A2").setString(f.read())
-#             firstSheet.getCellRangeByName("A3").setString('aaaa')
-#         firstSheet.getCellRangeByName("A4").setString('bbbb')
-#     except urllib.error.URLError as e:
-#         firstSheet.getCellRangeByName("A4").setString(e)
-#         return None
-      
 def fetch_data(url):
     url = "https://n8n.jcityoffice.com/webhook/78aaa2aa-bd89-44ad-bc59-9ba8db3e7d00?Div=Div1&month=April"
     response = requests.get(url)
