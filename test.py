e975deb1438a742258e15e62e4fd9c2423f091fc
ANKUSHJAIN91 <57315923+ANKUSHJAIN91@users.noreply.github.com>
2024-08-08 22:26:44 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,17 @@
 import uno
 import urllib.request
-# document = XSCRIPTCONTEXT.getDocument()
+document = XSCRIPTCONTEXT.getDocument()
 url = 'https://n8n.jcityoffice.com/webhook/78aaa2aa-bd89-44ad-bc59-9ba8db3e7d00?Div=Div1&month=April'
 
 print(urllib.request.urlopen(url))
-
 def write_something():
     month = "APRIL"
     div = "Div1"
-    # sheets = document.getSheets()
-    # firstSheet = sheets.getByIndex(0)
-    # firstSheet.getCellRangeByName("A1").setString('hhhhhh')
-    # firstSheet.getCellRangeByName("A4").setString('rrrrr')
-    # firstSheet.getCellRangeByName("A5").setString(urllib.request.urlopen(url))
+    sheets = document.getSheets()
+    firstSheet = sheets.getByIndex(0)
+    firstSheet.getCellRangeByName("A1").setString('hhhhhh')
+    firstSheet.getCellRangeByName("A4").setString('rrrrr')
+    firstSheet.getCellRangeByName("A5").setString(urllib.request.urlopen(url))
     print(urllib.request.urlopen(url))
     try:
         with urllib.request.urlopen(url) as f:
