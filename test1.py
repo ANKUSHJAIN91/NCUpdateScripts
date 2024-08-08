7c504a6a341fd54688c1a5ecd3c87277f6b61704
ANKUSHJAIN91 <57315923+ANKUSHJAIN91@users.noreply.github.com>
2024-08-08 20:12:43 +0530

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,45 @@
+import uno
+import urllib.request
+from urllib.parse import urlencode
+from urllib.request import urlopen
+import csv
+import json
+import sys
+import requests
+
+document = XSCRIPTCONTEXT.getDocument()
+# url = 'https://n8n.jcityoffice.com/webhook/78aaa2aa-bd89-44ad-bc59-9ba8db3e7d00?Div=Div1&month=April'
+
+# def write_something():
+#     month = "APRIL"
+#     div = "Div1"
+#     sheets = document.getSheets()
+#     firstSheet = sheets.getByIndex(0)
+#     firstSheet.getCellRangeByName("A1").setString('hhhhhh')
+#     firstSheet.getCellRangeByName("A4").setString('rrrrr')
+#     try:
+#         with urllib.request.urlopen(url) as f:
+#             # print(f.read(300))
+#             print(f.read())
+#             firstSheet.getCellRangeByName("A2").setString(f.read())
+#             firstSheet.getCellRangeByName("A3").setString('aaaa')
+#         firstSheet.getCellRangeByName("A4").setString('bbbb')
+#     except urllib.error.URLError as e:
+#         firstSheet.getCellRangeByName("A4").setString(e)
+#         return None
+      
+def fetch_data(url):
+    url = "https://n8n.jcityoffice.com/webhook/78aaa2aa-bd89-44ad-bc59-9ba8db3e7d00?Div=Div1&month=April"
+    response = requests.get(url)
+    if response.status_code == 200:
+        # data = json.loads(response.text)
+        data = response.text
+        sheets = document.getSheets()
+        firstSheet = sheets.getByIndex(0)
+        firstSheet.getCellRangeByName("A1").setString('hiwwqqii')
+        firstSheet.getCellRangeByName("A2").setString(data)
+        # Process the data as needed
+        return data
+    else:
+        print(f"Error fetching data: {response.status_code}")
+        return None   
